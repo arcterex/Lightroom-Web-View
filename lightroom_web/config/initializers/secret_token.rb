8032b9d7523afa50895a939024a6a4b7ab97db5e
Alan Bailward <alan@ufies.org>
2011-05-18 20:36:46 -0700

    Added in default rails3

---
+++ lightroom_web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LightroomWeb::Application.config.secret_token = '8e9cba1c635aa95e5925cd5722db7bb0f73e98836e143996c5d9f46d6013c8735615c925ee8626a92c323fe21b78fa35f5a6e0ffc9528126f48a7a204707b00b'
